16dd5b34b5c945010c6a19fe484be5680f9d4d71
aasim-s <aasimsayyed214@gmail.com>
2024-11-05 14:17:46 +0530

    structs in rust

--- 5_structs.rs
+++ 5_structs.rs
@@ -34,13 +34,13 @@ impl Rectangle {
         self.width * self.height
     }
 
-    fn can_hold(&self, &Rectangle) -> bool {
+    fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
 
     // is associated function but not a method as no self parameter in input
     // are called using ::
-    fn square(size: u32) -> self { 
+    fn square(size: u32) -> Self { 
         Self {
             width: size,
             height: size,
@@ -53,7 +53,7 @@ fn main() {
     let mut user1 = User {
         active: true,
         username: String::from("name123"),
-        email: String::("name@mail.com"),
+        email: String::from("name@mail.com"),
         sign_in_count: 1,
     };
     //each struct is its own type, even if the fields within might have same type
@@ -62,7 +62,7 @@ fn main() {
 
     let condition = AlwaysEqual;
 
-    let user2 = build_user("name@mail.com", "name345");
+    let user2 = build_user(String::from("name@mail.com"), String::from("name345"));
 
     // create a struct instance from another instance. this is called struct update syntax.
     // this moves the data from user2 to user1 so we can no longer use user1 as a whole again after this.
@@ -71,21 +71,21 @@ fn main() {
         active: user2.active,
         username: user2.username,
         email: String::from("name@mail.com"),
-        sign_in_count = user2.sign_in_count,
-    }
+        sign_in_count: user2.sign_in_count,
+    };
     // shorter syntax
     let user4 = User {
-        email: String::("name@mail.com"),
+        email: String::from("name@mail.com"),
         ..user1 //should come last to specify that any remaining fields should get their value from user1
-    }
+    };
 
-    user1.email = String::("fullname@mail.com");
+    user1.email = String::from("fullname@mail.com");
 
     let rect1 = Rectangle {
         width: 30,
         height: 50,
-    }
-    println!("Area of the rectangle is {}". rect1.area())
+    };
+    println!("Area of the rectangle is {}", rect1.area());
     let sq = Rectangle::square(13);
 
 }
